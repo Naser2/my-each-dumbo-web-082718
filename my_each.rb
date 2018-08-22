f399594a43a42d5120721be7f509b920c0ec18e9
Nasser Sanou <nassersanou23@gmail.com>
2018-08-22 20:49:08 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -4,8 +4,8 @@ collection = [1, 2, 3, 4]
 def my_each(collection) 
   num = 0
   while num < collection.length
-  yield collection[num] # yield every num
-  num += 1 #iterates next num
+        yield collection[num] # yield every num
+        num += 1 #iterates next num
   end 
   collection
 end
